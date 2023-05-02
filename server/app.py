70ea4bdc4fb5efef2822fc155e91963d6695bd51
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2023-05-02 15:19:50 +0100

    added predict route

--- server/app.py
+++ server/app.py
@@ -31,6 +31,27 @@ def test_predict():
 
     return prediction[0]
 
+@app.route('/predict', methods=['POST'])
+
+def predict():
+    data = request.get_json()
+
+    data = np.array(data)
+
+    sensor_data = data[0:len(data)-1]
+
+    user_data = data[-1]
+
+
+    # prediction = rocket_classifier.predict(sensor_data)
+
+    print(sensor_data)
+    # print(prediction)
+
+    # response = [prediction[0], prediction_probabilties[0]]
+
+    return "success"
+
 
 
 if __name__ == '__main__':
